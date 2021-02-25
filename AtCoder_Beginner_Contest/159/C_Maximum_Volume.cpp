9370e4d28e560c35ad02bdac16913977a0476b69
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2021-02-26 08:26:45 +0900

    159 ABC-C

---
+++ AtCoder_Beginner_Contest/159/C_Maximum_Volume.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main () {
+    double l;
+    cin >> l;
+    double div3 = l / 3;
+    cout << fixed << setprecision(15) << div3 * div3 * div3 << endl;
+    return 0;
+}
