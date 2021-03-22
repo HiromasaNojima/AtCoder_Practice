181e8b50db642213a8bb45475af00cf43a1f8c81
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2021-03-22 19:50:45 +0900

    196 ABC-A

---
+++ AtCoder_Beginner_Contest/196/A_Difference_Max.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b - c << endl;
+    return 0;
+}
