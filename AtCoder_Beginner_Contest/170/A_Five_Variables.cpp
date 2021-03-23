cb47af0c2b646c5a14980075494ac66d1b3d4ab1
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2021-03-23 19:01:27 +0900

    170 ABC-A

---
+++ AtCoder_Beginner_Contest/170/A_Five_Variables.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+    rep(i, 5) {
+        int x;
+        cin >> x;
+        if (x == 0) {
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
